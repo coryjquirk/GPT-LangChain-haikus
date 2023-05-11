0b6bec1b10070fba8027aa1a844d61c9c8fe30d2
Cory Quirk <coryjquirk@gmail.com>
2023-05-11 16:42:56 -0500

    troubleshooting haiku generation - chaining prompts to check syllable counts

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import os
+import os, time
 from apikey import apikey
 
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SequentialChain
+from langchain.chains import LLMChain
 from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper
 
@@ -13,38 +13,71 @@ os.environ['OPENAI_API_KEY'] = apikey
 st.title('GPT haiku creator')
 prompt = st.text_input('Write a haiku about:')
 
-title_template = PromptTemplate(
+haiku_template = PromptTemplate(
     input_variables = ['topic'],
     template = 'Write me a haiku about {topic}'
 )
 
-haiku_template = PromptTemplate(
-    input_variables = ['title', 'wiki_research'],
-    template = 'Write me a haiku based on this title: {title} while leveraging this Wikipedia research: {wiki_research}'
+informed_haiku_template = PromptTemplate(
+    input_variables = ['haiku', 'wiki_research'],
+    template = 'Write me a haiku based on this haiku: {haiku} while leveraging this Wikipedia research: {wiki_research}'
 )
 
-title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
-haiku_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
+syllable_counter_template = PromptTemplate(
+    input_variables = ['haiku'],
+    template = 'How many total syllables are in the following words? {haiku}'
+)
+
+haiku_checker_template = PromptTemplate(
+    input_variables = ['haiku'],
+    template = 'Does the following poem fit the definition of a haiku?: {haiku}'
+)
+
+haiku_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
+informed_haiku_memory = ConversationBufferMemory(input_key='haiku', memory_key='chat_history')
+syllable_counter_memory = ConversationBufferMemory(input_key='haiku', memory_key='chat_history')
+haiku_checker_memory = ConversationBufferMemory(input_key='haiku', memory_key='chat_history')
 
 llm = OpenAI(temperature = 0.9)
-title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
 haiku_chain = LLMChain(llm=llm, prompt=haiku_template, verbose=True, output_key='haiku', memory=haiku_memory)
+informed_haiku_chain = LLMChain(llm=llm, prompt=informed_haiku_template, verbose=True, output_key='informed_haiku', memory=informed_haiku_memory)
+syllable_counter_chain = LLMChain(llm=llm, prompt=syllable_counter_template, verbose=True, output_key='syllable_count', memory=syllable_counter_memory)
+haiku_checker_chain = LLMChain(llm=llm, prompt=haiku_checker_template, verbose=True, output_key='haiku_checker', memory=haiku_checker_memory)
 
 wiki = WikipediaAPIWrapper()
 
 if prompt:
-    title = title_chain.run(prompt)
-    wiki_research = wiki.run(prompt)
-    haiku = haiku_chain.run(title=title, wiki_research=wiki_research)
+    haiku = haiku_chain.run(prompt)
+    # time.sleep(2)
+    # wiki_research = wiki.run(prompt)
+    # informed_haiku = informed_haiku_chain.run(haiku=haiku, wiki_research=wiki_research)
+    time.sleep(2)
+    haiku_syllable_count = syllable_counter_chain.run(haiku=haiku)
+    time.sleep(2)
+    # informed_haiku_syllable_count = syllable_counter_chain.run(haiku=informed_haiku)
+    time.sleep(2)
+    haiku_checker = haiku_checker_chain.run(haiku=haiku)
+    time.sleep(2)
+    # informed_haiku_checker = haiku_checker_chain.run(haiku=informed_haiku)
 
-    st.write(title)
-    st.write(haiku)
-
-    with st.expander('Title History'):
-        st.info(title_memory.buffer)
+    st.write("Haiku: " + haiku)
+    # st.write("Informed haiku: " + informed_haiku)
+    st.write("Haiku syllable count: " + haiku_syllable_count)
+    # st.write("Informed haiku syllable count: " + informed_haiku_syllable_count)
+    st.write("Is the AI's haiku a haiku?" + haiku_checker)
+    # st.write("Is the AI's informed haiku a haiku?" + informed_haiku_checker)
 
     with st.expander('Haiku History'):
         st.info(haiku_memory.buffer)
 
-    with st.expander('Wikipedia Research'):
-        st.info(wiki_research)
+    with st.expander('Informed Haiku History'):
+        st.info(informed_haiku_memory.buffer)
+
+    # with st.expander('Wikipedia Research'):
+    #     st.info(wiki_research)
+
+    with st.expander('Syllable history'):
+        st.info(syllable_counter_memory.buffer)
+
+    with st.expander('Checker history:'):
+        st.info(haiku_checker_memory.buffer)
