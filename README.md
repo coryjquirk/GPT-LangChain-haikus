fb417f50df28fad450839dda453d2a9ebcec866e
Cory Quirk <coryjquirk@gmail.com>
2023-05-18 15:32:41 -0500

    giving AI haiku examples in prompt for better results. still WIP

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ tiktoken - backend tokenizer for openai
 2. Set your secret key to a variable called `api_key` in a file called `apikey.py` at the root of this repository.
 
 # Usage
-`streamlit run app.py`
+`python -m streamlit run app.py`
 
 # Credit
 Produced with the guidance of [Nicholas Renotte](https://github.com/nicknochnack)'s [LangChain Crash Course: Build a AutoGPT app in 25 minutes!](https://www.youtube.com/watch?v=MlK6SIjcjE8)
--- app.py
+++ app.py
@@ -11,11 +11,52 @@ from langchain.utilities import WikipediaAPIWrapper
 os.environ['OPENAI_API_KEY'] = apikey
 
 st.title('GPT haiku creator')
-prompt = st.text_input('Write a haiku about:')
+prompt = st.text_input('Write me a haiku about:')
 
 haiku_template = PromptTemplate(
     input_variables = ['topic'],
-    template = 'Write me a haiku about {topic}'
+    template = '''
+    Write a traditional haiku for me.
+    A traditional haiku is comprised of three lines of 5, 7, and 5 syllables.
+
+    A syllable is a unit of spoken language that is next bigger than a speech sound 
+    and consists of one or more vowel sounds alone or of a syllabic consonant alone 
+    or of either with one or more consonant sounds preceding or following. 
+
+    Here are some examples:
+
+    Q: Write me a haiku about video games.
+
+    A: 
+    Late nights with my friends
+    A·comm-plish im·poss·ible
+    Dream·ing on my screen
+
+    Q: Write me a haiku about how it feels to sit by a fire.
+
+    A: 
+    Pop crackl·ing sounds
+    Smoke stink brings back me·mo·ries
+    Glow·ing logs com·fort
+
+    Q: Write me a haiku about San Francisco in the summer time.
+
+    A: 
+    At Gol-den Gate Park
+    Mu·sic flow·ing through the air    
+    People rejoi·cing
+
+    Q: Write me a haiku about the taste of pizza.
+
+    A:
+    Nos·trils a·wa·ken 
+    Va·por·ous de·li-cious smells 
+    Savo·ry good·ness
+
+    Q: Write me a haiku about {topic}.
+    
+    A:
+    '''
 )
 
 informed_haiku_template = PromptTemplate(
@@ -25,7 +66,9 @@ informed_haiku_template = PromptTemplate(
 
 syllable_counter_template = PromptTemplate(
     input_variables = ['haiku'],
-    template = 'How many total syllables are in the following words? {haiku}'
+    template = ''' 
+    How many total syllables are in the following words? {haiku}
+    '''
 )
 
 haiku_checker_template = PromptTemplate(
@@ -48,16 +91,11 @@ wiki = WikipediaAPIWrapper()
 
 if prompt:
     haiku = haiku_chain.run(prompt)
-    # time.sleep(2)
     # wiki_research = wiki.run(prompt)
     # informed_haiku = informed_haiku_chain.run(haiku=haiku, wiki_research=wiki_research)
-    time.sleep(2)
     haiku_syllable_count = syllable_counter_chain.run(haiku=haiku)
-    time.sleep(2)
     # informed_haiku_syllable_count = syllable_counter_chain.run(haiku=informed_haiku)
-    time.sleep(2)
     haiku_checker = haiku_checker_chain.run(haiku=haiku)
-    time.sleep(2)
     # informed_haiku_checker = haiku_checker_chain.run(haiku=informed_haiku)
 
     st.write("Haiku: " + haiku)
